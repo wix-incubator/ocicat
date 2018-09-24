84042e0dddb0451942f4854c2568964fb0f8b961
valentynv <valentynv@wix.com>
2018-09-24 16:53:45 +0300

    Fix critical issue with tick calculation

--- src/main/scala/com/wix/ocicat/Throttler.scala
+++ src/main/scala/com/wix/ocicat/Throttler.scala
@@ -12,7 +12,6 @@ trait Throttler[F[_], A] {
 
 object Throttler {
 
-
   sealed trait ThrottleStatus
 
   case object Throttled extends ThrottleStatus
@@ -32,21 +31,21 @@ object Throttler {
         override def throttle(key: A) = for {
           now <- T.clock.realTime(TimeUnit.MILLISECONDS)
           currentTick = now / window
-          throttleStatus <- state.modify {
-            case (previousTick, counts) =>
-              // todo validate limit
-              if (currentTick > previousTick) {
-                ((now, Map(key -> 1)), NonThrottled)
-              } else {
-                counts.get(key) match {
-                  case Some(count) => if (count + 1 > limit) {
+          throttleStatus <- state.modify { case (previousTick, counts) =>
+            // todo validate limit
+            if (currentTick > previousTick) {
+              ((currentTick, Map(key -> 1)), NonThrottled)
+            } else {
+              counts.get(key) match {
+                case Some(count) =>
+                  if (count + 1 > limit) {
                     ((previousTick, counts + (key -> (count + 1))), Throttled)
                   } else {
                     ((previousTick, counts + (key -> (count + 1))), NonThrottled)
                   }
-                  case None => ((previousTick, counts + (key -> 1)), NonThrottled)
-                }
+                case None => ((previousTick, counts + (key -> 1)), NonThrottled)
               }
+            }
           }
           _ <- throttleStatus match {
             case Throttled => S.raiseError(new RuntimeException(s"key $key is throttled"))
