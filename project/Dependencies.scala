292b53c632df562471fc354d51f8c2c3b54de7ec
Dmitriy Mayboroda <dmytromay@wix.com>
2020-07-03 13:00:44 +0300

    Change project structure in order to support more flexible way for external dependencies Add doobie dummy project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,4 +3,8 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.0.0"
+
+  lazy val doobieCore = "org.tpolecat" %% "doobie-core" % "0.8.8"
+  lazy val doobieH2 = "org.tpolecat" %% "doobie-h2" % "0.8.8"
+  lazy val doobieTest = "org.tpolecat" %% "doobie-scalatest" % "0.8.8"
 }
